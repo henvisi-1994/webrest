dd3f00c895ee7ec20da5ad78f16d79cad61a77bf
Marvin Valarezo <marvinv572@gmail.com>
2020-12-03 23:12:51 -0500

    modelos ricardo

---
+++ restaurante/src/app/models/PedidoProveedor.ts
@@ -0,0 +1,7 @@
+export interface PedidoProveedor {
+  id_dedido_prov: number;
+  cantidad_pedido_prov: number;
+  Fecha_caducidad_ped_prov: Date;
+  id_prov: number;
+  id_local: number;
+}
---
+++ restaurante/src/app/models/Telefono.ts
@@ -0,0 +1,5 @@
+export interface Telefono {
+  id_telefono: number;
+  numero_telf: String;
+  id_local: number;
+}
