dffa25fbc1cfc8c4d4fa23efa22d5e00f264c55b
henvisi-1994 <henvisi_1994@hotmail.com>
2020-12-04 01:31:22 -0500

    Servicios API 4

--- restaurante-api/src/main/java/Inventario/Services/PedidoProveedorServicesRS.java
+++ restaurante-api/src/main/java/Inventario/Services/PedidoProveedorServicesRS.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package InventarioServices;
+package Inventario.Services;
 import Inventario.Interfaces.PedidoProveedorDao;
 import Inventario.PedidoProveedor;
 import java.util.List;
--- restaurante-api/src/main/java/Inventario/Services/PedidosServiceRS.java
+++ restaurante-api/src/main/java/Inventario/Services/PedidosServiceRS.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package InventarioServices;
+package Inventario.Services;
 
 import Inventario.Interfaces.PedidoDao;
 import Inventario.Pedidos;
